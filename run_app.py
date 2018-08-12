dcb4ff7917d498072e24b59db7885b36a9b1bf04
onlyphantom <samuel@outlook.sg>
2018-08-12 17:39:26 +0700

    Display mathematician's country with db joins

--- run_app.py
+++ run_app.py
@@ -1,5 +1,6 @@
 # from library.app_part_01 import app
-from library.app_part_02 import app
+# from library.app_part_02 import app
+from library.app_part_03 import app
 
 if __name__ == '__main__':
     app.run(debug=True)
